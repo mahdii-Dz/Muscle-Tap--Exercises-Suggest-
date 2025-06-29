d171c6b8e5777724901a163ebbe508c127054925
mahdi <hadjidjmahdi@gmail.com>
2025-06-29 21:26:31 +0100

    adding list sort functionality

--- app/components/SuggestedExercises.jsx
+++ app/components/SuggestedExercises.jsx
@@ -5,7 +5,9 @@ import useExercises from './useExercises'
 import Loading from './Loading'
 import Image from 'next/image'
 import { Tooltip } from '@/components/ui/tooltip'
-import { TooltipContent, TooltipProvider, TooltipTrigger } from '@radix-ui/react-tooltip'
+import { TooltipContent, TooltipTrigger } from '@radix-ui/react-tooltip'
+import SortableList, { SortableItem, SortableKnob } from 'react-easy-sort'
+import { arrayMoveImmutable } from 'array-move'
 
 function SuggestedExercises() {
     const { muscles } = useContext(GlobalContext)
@@ -29,42 +31,53 @@ function SuggestedExercises() {
 
     function handleDelete(index) {
         setExercises((prev) => prev.filter((_, i) => i !== index))
-
     }
+
+    const onSortEnd = (oldIndex, newIndex) => {
+        setExercises((array) => arrayMoveImmutable(array, oldIndex, newIndex))
+    }
+
+    console.log(exercises);
 
     return (
         <section className='w-full h-auto py-12 px-8 mb-16'>
             <h1 className='text-xl font-semibold mb-8'>Exercises</h1>
-            <div className="wrapper">
+            <SortableList onSortEnd={onSortEnd} className="list" draggedItemClassName='dragged' lockAxis='y'>
                 {
                     exercises
                         ? exercises.map((exo, index) => (
-                            <div key={index} className='w-full h-16 flex bg-gray-800 px-4 mb-2 rounded-lg justify-between items-center'>
-                                <div className="dragIcon flex gap-2 items-center">
-                                    <Image src={'/grip-vertical.svg'} alt='grab icon' width={20} height={20} className='cursor-grab' />
-                                    <span className='bg-primary-1 text-white text-sm font-semibold rounded-sm px-2 py-1'>{exo.muscle}</span>
-                                    <Tooltip>
-                                        <TooltipTrigger asChild>
-                                            <Image src={'/info.svg'} alt='info icon' width={20} height={20} className='cursor-pointer' />
-                                        </TooltipTrigger>
-                                        <TooltipContent>
-                                            <p className='bg-white/50 text-sm px-2 py-1 rounded-sm'>instructions</p>
-                                        </TooltipContent>
-                                    </Tooltip>
+                            <SortableItem key={index} >
+                                <div className='item w-full h-16 flex bg-gray-800 px-4 mb-2 select-none rounded-lg justify-between items-center'>
+                                    <div className="dragIcon flex gap-2 items-center ">
+                                        <SortableKnob>
+                                            <div className='p-1 cursor-grab'>
+                                                <Image src={'/grip-vertical.svg'} alt='grab icon' width={20} height={20} className='cursor-grab pointer-events-none' draggable={false} />
+                                            </div>
+                                        </SortableKnob>
+                                        <span className='bg-primary-1 text-white text-sm font-semibold rounded-sm px-2 py-1'>{exo.muscle}</span>
+                                        <Tooltip>
+                                            <TooltipTrigger asChild>
+                                                <Image src={'/info.svg'} alt='info icon' width={20} height={20} className='cursor-pointer' />
+                                            </TooltipTrigger>
+                                            <TooltipContent>
+                                                <p className='bg-white/50 text-sm px-2 py-1 rounded-sm'>instructions</p>
+                                            </TooltipContent>
+                                        </Tooltip>
+                                    </div>
+                                    <div>
+                                        <span>{exo.name}</span>
+                                    </div>
+                                    <div>
+                                        <button onClick={() => handleDelete(index)}>
+                                            <Image src={'/trash.svg'} alt='delete icon' width={20} height={20} className='opacity-85 cursor-pointer' />
+                                        </button>
+                                    </div>
                                 </div>
-                                <div>
-                                    <span>{exo.name}</span>
-                                </div>
-                                <div>
-                                    <button onClick={() => handleDelete(index)}>
-                                        <Image src={'/trash.svg'} alt='delete icon' width={20} height={20} className='opacity-85 cursor-pointer' />
-                                    </button>
-                                </div>
-                            </div>
+                            </SortableItem>
                         ))
                         : <span>No Exercises found</span>
                 }
-            </div>
+            </SortableList>
         </section>
     )
 }
