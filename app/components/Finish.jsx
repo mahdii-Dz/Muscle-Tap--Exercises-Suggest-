4aab53f2c9b04f073b9e33e06a3143a2ee340afc
mahdi <hadjidjmahdi@gmail.com>
2025-06-30 19:23:07 +0100

    responsive fix

--- app/components/Finish.jsx
+++ app/components/Finish.jsx
@@ -6,7 +6,7 @@ function Finish({customClassName}) {
     <div className={`w-full h-full flex justify-center items-center ${customClassName}`}>
         <div className='w-2/3 h-7/12 bg-white rounded-lg flex flex-col justify-center items-center'> 
             <Image loading='lazy' src={'/Congrates.gif'} alt='congratulations gif' width={200} height={200}  />
-            <h3 className='text-black text-2xl font-bold'>Congratulations🎉</h3>
+            <h3 className='text-black text-2xl  font-bold'>Congratulations🎉</h3>
         </div>
     </div>
   )
--- app/sections/StepsBar.jsx
+++ app/sections/StepsBar.jsx
@@ -7,41 +7,41 @@ function StepsBar() {
 
 
   return (
-    <aside className='sm:w-2/6 w-full sm:h-full h-2/4 sm:pl-0 pl-4 py-16 bg-gray-800 flex flex-col justify-start items-start sm:items-center'>
-      <div className="wrapper">
-        <div className='First-step flex items-center w-full gap-4'>
-          <div className={`number w-12 h-12 ${pageNum === 1 ? 'bg-primary-1' : 'bg-green-400'} text-white text-lg rounded-full flex justify-center items-center`}>
+    <aside className='sm:w-2/6 w-full sm:h-full h-1/4 pb-16 pt-4 sm:pt-16 bg-gray-800 flex flex-col justify-start items-start sm:items-center'>
+      <div className="wrapper flex sm:flex-col flex-row justify-center ">
+        <div className='First-step flex sm:flex-row flex-col items-center  gap-4'>
+          <div className={`number sm:w-12 w-8 sm:h-12 h-8 ${pageNum === 1 ? 'bg-primary-1' : 'bg-green-400'} text-white text-lg rounded-full flex justify-center items-center`}>
             {
               pageNum === 1 ? <span>1</span> : <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style={{ fill: 'rgba(255, 255, 255, 1)' }}><path d="m10 15.586-3.293-3.293-1.414 1.414L10 18.414l9.707-9.707-1.414-1.414z"></path></svg>
             }
           </div>
-          <div className='flex flex-col items-start justify-center'>
-            <span className={`text-lg font-bold ${pageNum === 1 ? 'text-white' : 'text-green-400'}`}>Muscles</span>
+          <div className='flex flex-col sm:items-start items-center text-center justify-center'>
+            <span className={`sm:text-lg font-bold ${pageNum === 1 ? 'text-white' : 'text-green-400'}`}>Muscles</span>
             <span className='text-gray-500'>Choose your training</span>
           </div>
         </div>
-        <div className={`w-0.5 sm:h-24 h-16 ml-6 my-4  ${pageNum === 1 ? 'bg-gray-500' : 'bg-green-400'}`}></div>
-        <div className='second-step flex items-center w-full gap-4'>
-          <div className={`number w-12 h-12 ${pageNum <= 2 ? 'bg-primary-1' : 'bg-green-400'} text-white text-lg rounded-full flex justify-center items-center`}>
+        <div className={`sm:w-0.5 w-24 sm:h-24 h-0.5 sm:ml-6 my-4  ${pageNum === 1 ? 'bg-gray-500' : 'bg-green-400'}`}></div>
+        <div className='second-step flex sm:flex-row flex-col items-center  gap-4'>
+          <div className={`number sm:w-12 w-8 sm:h-12 h-8 ${pageNum <= 2 ? 'bg-primary-1' : 'bg-green-400'} text-white text-lg rounded-full flex justify-center items-center`}>
             {
               pageNum <= 2 ? <span>2</span> : <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style={{ fill: 'rgba(255, 255, 255, 1)' }}><path d="m10 15.586-3.293-3.293-1.414 1.414L10 18.414l9.707-9.707-1.414-1.414z"></path></svg>
             }
           </div>
-          <div className='flex flex-col items-start justify-center'>
-            <span className='text-lg font-bold'>Exercises</span>
+          <div className='flex flex-col sm:items-start items-center text-center justify-center'>
+            <span className='tsm:ext-lg font-bold'>Exercises</span>
             <span className='text-gray-500'>Customize your workout</span>
           </div>
         </div>
 
-        <div className={`w-0.5 sm:h-24 h-16 ml-6 my-4  ${pageNum <= 2 ? 'bg-gray-500' : 'bg-green-400'}`}></div>
-        <div className='second-step flex items-center w-full gap-4'>
-          <div className={`number w-12 h-12 ${pageNum <= 3 ? 'bg-primary-1' : 'bg-green-400'} text-white text-lg rounded-full flex justify-center items-center`}>
+        <div className={`sm:w-0.5 w-24 sm:h-24 h-0.5 ml-6 my-4  ${pageNum <= 2 ? 'bg-gray-500' : 'bg-green-400'}`}></div>
+        <div className='third-step flex sm:flex-row flex-col items-center  gap-4'>
+          <div className={`number sm:w-12 w-8 sm:h-12 h-8 ${pageNum <= 3 ? 'bg-primary-1' : 'bg-green-400'} text-white text-lg rounded-full flex justify-center items-center`}>
             {
               pageNum <= 3 ? <span>3</span> : <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style={{ fill: 'rgba(255, 255, 255, 1)' }}><path d="m10 15.586-3.293-3.293-1.414 1.414L10 18.414l9.707-9.707-1.414-1.414z"></path></svg>
             }
           </div>
-          <div className='flex flex-col items-start justify-center'>
-            <span className='text-lg font-bold'>Finish</span>
+          <div className='flex flex-col sm:items-start items-center text-center justify-center'>
+            <span className='tsm:ext-lg font-bold'>Finish</span>
             <span className='text-gray-500'>Start Your WorkOut</span>
           </div>
         </div>
